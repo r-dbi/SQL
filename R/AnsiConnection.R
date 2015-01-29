f6ebd3a20e9192dec83bec4937b33162e3e6bcb4
hadley <h.wickham@gmail.com>
2015-01-29 07:49:21 -0600

    Move ANSI into DBI package

--- R/AnsiConnection.R
+++ R/AnsiConnection.R
@@ -1,47 +1,15 @@
 #' @importClassesFrom DBI DBIConnection DBIObject
 #' @useDynLib SQL
 #' @importFrom Rcpp sourceCpp
-setClass("AnsiConnection", contains = "DBIConnection")
+NULL
 
-#' A dummy DBI connector that simulates ANSI-SQL compliance
+# Moved into DBI
+
+
+#' Create (dummy) ANSI connection
 #'
-#' @export
 #' @keywords internal
-#' @examples
-#' ANSI()
-ANSI <- function() {
-  new("AnsiConnection")
-}
-
-#' @export
-#' @rdname ANSI
-setMethod("show", "AnsiConnection", function(object) {
-  cat("<AnsiConnection>\n")
-})
-
-#' @importFrom DBI dbDataType
-#' @export
-#' @rdname ANSI
-setMethod("dbDataType", c("AnsiConnection", "integer"),  function(dbObj, obj, ...) "INTEGER")
-#' @export
-#' @rdname ANSI
-setMethod("dbDataType", c("AnsiConnection", "numeric"),  function(dbObj, obj, ...) "DOUBLE")
-#' @export
-#' @rdname ANSI
-setMethod("dbDataType", c("AnsiConnection", "logical"),  function(dbObj, obj, ...) "SMALLINT")
-#' @export
-#' @rdname ANSI
-setMethod("dbDataType", c("AnsiConnection", "Date"),     function(dbObj, obj, ...) "DATE")
-#' @export
-#' @rdname ANSI
-setMethod("dbDataType", c("AnsiConnection", "POSIXct"),  function(dbObj, obj, ...) "TIMESTAMP")
-varchar <- function(dbObj, obj, ...) {
-  paste0("VARCHAR(", max(nchar(as.character(obj))), ")")
-}
-#' @export
-#' @rdname ANSI
-setMethod("dbDataType", c("AnsiConnection", "character"), varchar)
-#' @export
-#' @rdname ANSI
-setMethod("dbDataType", c("AnsiConnection", "factor"),    varchar)
-
+#' @importFrom DBI ANSI
+#' @export ANSI
+#' @name ANSI
+NULL
